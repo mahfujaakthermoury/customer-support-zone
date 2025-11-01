b2aeaeb5453729567d838a24bd9a4262d79a949b
majfujamoury <mahfujaakther186630gmail.com>
2025-11-01 22:14:19 +0600

    ffinal

--- src/Component/Banner/Banner.jsx
+++ src/Component/Banner/Banner.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
-import vectorTwo from "../../assets/vectorTwo.png"
+//import vectorTwo from "../../assets/vectorTwo.png"
 
-const Banner = ({inProgress, resolved}) => {
+const Banner = ({ inProgress, resolved }) => {
 
 
 
   return (
     <div className='max-w-[1200px] mx-auto my-15 flex text-white text-2xl'>
       <div className='grid place-content-center h-[220px] w-[650px] rounded-xl
-        mr-5 ' style={{
-    backgroundImage: `linear-gradient(to right, rgb(99 46 227), rgb(159 98 242), url(${vectorTwo})`,
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-  }} >
+        mr-5 bg bg-linear-65 from-[#632EE3] to-[#9F62F2]'
+      //       style={{
+      //   backgroundImage: `linear-gradient(to right, rgb(99 46 227), rgb(159 98 242), url(${vectorTwo})`,
+      //   backgroundSize: 'cover',
+      //   backgroundPosition: 'center',
+      // }} 
+      >
         <h1>In Progress</h1>
         <h1 className='text-6xl font-semibold pt-4 ps-11'>{inProgress.length}</h1>
       </div>
