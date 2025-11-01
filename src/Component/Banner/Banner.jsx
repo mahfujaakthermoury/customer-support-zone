4aebbdb40f1ba5c21e589ddbcb497850b94cfc48
majfujamoury <mahfujaakther186630gmail.com>
2025-11-02 00:17:03 +0600

    update

--- src/Component/Banner/Banner.jsx
+++ src/Component/Banner/Banner.jsx
@@ -1,25 +1,26 @@
 import React from 'react';
-//import vectorTwo from "../../assets/vectorTwo.png"
+//import vectorTwo from "../../assets/vectortwo.png"
 
 const Banner = ({ inProgress, resolved }) => {
 
-
+//bg-[linear-gradient(65deg,#632EE3,#9F62F2),url('/path/to/image.png')] bg-cover bg-center bg-blend-overlay
 
   return (
     <div className='max-w-[1200px] mx-auto my-15 flex text-white text-2xl'>
+
       <div className='grid place-content-center h-[220px] w-[650px] rounded-xl
-        mr-5 bg bg-linear-65 from-[#632EE3] to-[#9F62F2]'
-      //       style={{
-      //   backgroundImage: `linear-gradient(to right, rgb(99 46 227), rgb(159 98 242), url(${vectorTwo})`,
-      //   backgroundSize: 'cover',
-      //   backgroundPosition: 'center',
-      // }} 
-      >
-        <h1>In Progress</h1>
+        mr-5 bg-[linear-gradient(65deg,#632EE3,#9F62F2)] bg-cover bg-center relative'>
+
+          <img src="/image/vector.png" alt="" className="absolute"  />
+          <img src="/image/vector.png" alt="" className="absolute right-0 transform scale-x-[-1] "/>
+          <h1>In Progress</h1>
         <h1 className='text-6xl font-semibold pt-4 ps-11'>{inProgress.length}</h1>
-      </div>
+        </div>
+
       <div className='grid place-content-center h-[220px] w-[650px]  
-      rounded-xl bg bg-linear-65 from-[#16f759] to-[#049b54]'>
+      rounded-xl bg-[linear-gradient(65deg,#16f759,#049b54)] bg-cover bg-center relative'>
+         <img src="/image/vector.png" alt="" className="absolute"  />
+          <img src="/image/vector.png" alt="" className="absolute right-0 transform scale-x-[-1] "/>
         <h1>Resolved</h1>
         <h1 className='text-6xl font-semibold pt-4 ps-8'>{resolved.length}</h1>
       </div>
